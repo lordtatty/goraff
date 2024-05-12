dc483dbd82969d159e18f489db313cecebb98011
LT <lordtatty@googlemail.com>
2024-05-12 15:15:52 +0100

    fix action expectations

--- nodeactions/print.go
+++ nodeactions/print.go
@@ -9,7 +9,7 @@ import (
 type Print struct {
 }
 
-func (p *Print) Do(s *goraff.StateNode, r *goraff.GraphStateReader, triggeringNode *goraff.StateNode) error {
-	log.Printf("Node triggered by %s\n", triggeringNode.Reader().ID())
+func (p *Print) Do(s *goraff.StateNode, r *goraff.GraphStateReader, triggeringNode *goraff.StateNodeReader) error {
+	log.Printf("Node triggered by %s\n", triggeringNode.ID())
 	return nil
 }
